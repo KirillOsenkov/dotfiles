4b45b3989df4c38ecb111ede89c76d25005ba464
Kirill Osenkov <KirillOsenkov@users.noreply.github.com>
2023-11-17 11:59:12 -0800

    Create DotnetTools.cmd

---
+++ DotnetTools.cmd
@@ -0,0 +1,7 @@
+dotnet tool update -g binlogtool
+dotnet tool update -g checkbinarycompat
+dotnet tool update -g contentsync
+dotnet tool update -g ff
+dotnet tool update -g lbi
+dotnet tool update -g refdump
+dotnet tool update -g timing
