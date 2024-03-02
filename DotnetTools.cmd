61fd969cd3addea5e3724e886c1456c197e7b3c7
Kirill Osenkov <KirillOsenkov@users.noreply.github.com>
2024-03-01 17:56:55 -0800

    Add pdb

--- DotnetTools.cmd
+++ DotnetTools.cmd
@@ -5,6 +5,7 @@ dotnet tool update -g checkbinarycompat
 dotnet tool update -g contentsync
 dotnet tool update -g ff
 dotnet tool update -g lbi
+dotnet tool update -g pdb
 dotnet tool update -g refdump
 dotnet tool update -g sha
 dotnet tool update -g timing
