a05a2d859a5db6a88398c104921c854dc18645fb
Kirill Osenkov <KirillOsenkov@users.noreply.github.com>
2024-07-31 14:54:16 -0700

    Update DotnetTools.cmd

--- DotnetTools.cmd
+++ DotnetTools.cmd
@@ -3,6 +3,7 @@ REM https://mastodon.social/@KirillOsenkov/111427666886735396
 dotnet tool update -g binlogtool
 dotnet tool update -g checkbinarycompat
 dotnet tool update -g contentsync
+dotnet tool update -g dotnet-ilrepack
 dotnet tool update -g ff
 dotnet tool update -g lbi
 dotnet tool update -g pdb
