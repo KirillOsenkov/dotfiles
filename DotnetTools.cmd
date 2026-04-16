c785292d1f4e5ce3d1902b4f65ee4a17829221ba
Kirill Osenkov <KirillOsenkov@users.noreply.github.com>
2026-04-15 22:57:21 -0700

    add beetle

--- DotnetTools.cmd
+++ DotnetTools.cmd
@@ -1,5 +1,6 @@
 REM https://mastodon.social/@KirillOsenkov/111427666886735396
 
+dotnet tool update -g beetle
 dotnet tool update -g binlogtool
 dotnet tool update -g checkbinarycompat
 dotnet tool update -g contentsync
